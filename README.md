63772170f53ca32639c64fc3fcc1e38d11d2e795
warroyo <warroyo7199008@gmail.com>
2025-10-01 12:47:25 -0600

    initial work for v2

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
 # ArgoCD auto Attach Service
 
-This supervisor service can be used to automatically add workload clusters to ArgoCD as they are deployed or after the fact by using a label on the cluster. This is currently paired with this [ArgoCD service](https://vsphere-tmm.github.io/Supervisor-Services/#argocd-operator).
+This supervisor service can be used to automatically add workload clusters to ArgoCD as they are deployed or after the fact . This is currently paired with this [ArgoCD service](https://vsphere-tmm.github.io/Supervisor-Services/#argocd-operator).
 
 
 ## How it works
 
-ArgoCD currently runs centralized in the supervisor cluster. When deploying workload clusters by default these clusters are not registered back to ArgoCD. This supervsior service handles that by introducing a controller that watches for clusters being created that have the label `argocd-attach`. When a cluster is detected it will generate a secret that matches the ArgoCD secret spec for a cluster and use the contents of the kubeconfig for that cluster to generate that secret. This secret is managed with the cluster's lifecycle so it will also be cleaned up if a clusteris deleted.
+ArgoCD currently runs centralized in the supervisor cluster. When deploying workload clusters by default these clusters are not registered back to ArgoCD. This supervsior service handles that by introducing a controller that has a new CRD called `ArgoCluster`. This CRD can be created alongside any cluster and it will register that cluster with the argocd instance in the specified namespace. Behind the scenes this controller will create the secret necessary to register the cluster to argo.
 
 ## Install
 
 1. login into VCenter and go to the worload management->services page
 2. add a new service and upload the argo-attach.yml
-3. add any additional values that are needed
+3. add any additional values that are needed. see the values section below.
 4. install
+
+
+## Values
+| Field               | Value         | Description |
+|--------------------|---------------|-------------|
+| `resync_period`     | `60`          | Resync period in seconds |
+| `namespace`         | `""`          | namespace to deploy into, this is filled by the supervisor do not edit|
+| `blocked_namespaces`| `[""]`        | Namespaces that should not be allowed |
 
 ## AirGap Install
 
 1. relocate the image bundle to you repository, grab the latest imaeg bundle from the `argo-atatch.yml`
 
 ```bash
-imgpkg copy -b ghcr.io/warroyo/argocd-auto-attach@sha256:0fccfa38786c1a99354149140560683cc65c52e2351dc5fa145741b6701e78b7 --to-repo your-repo.com/argocd-auto-attach
+imgpkg copy -b <latest bundle from argo-attach.yml> --to-repo your-repo.com/argocd-auto-attach
 ```
 
 2. replace the image bundle in the `argo-atatch.yml` with your new path to the image bundle. the SHA should still be the same so all you should need to replace is the `ghcr.io/warroyo/argocd-auto-attach` with your repo and path.
@@ -31,35 +39,11 @@ imgpkg copy -b ghcr.io/warroyo/argocd-auto-attach@sha256:0fccfa38786c1a993541491
 
 Once the service is installed and also ArgoCD is installed. Follow these steps
 
-1. Login to ArgoCD and and create a new app with the below settings or apply this application below into the supervisor namespace that ArgoCD is running in.
-
-```yaml
-apiVersion: argoproj.io/v1alpha1
-kind: Application
-metadata:
-  name: clusters
-  namespace: ns01
-spec:
-  destination:
-    namespace: ns01
-    server: https://kubernetes.default.svc
-  project: default
-  source:
-    path: examples
-    repoURL: https://github.com/warroyo/argocd-attach-service
-    targetRevision: HEAD
-  syncPolicy:
-    automated: {}
-```
-
-2. Check and ensure the app is syncing in the Argo console. 
-3. once the cluster starts creating, navigate to Settings->Clusters in the Argo console and you will see a new entry for the newly created cluster
-
-![alt text](images/image.png)
+1. update the yaml in the `examples/argoCluster.yml` with your details
+2. `kubectl apply -f examples/argoCluster.yml`
 
 ## Development
  
-Due to a limitation with Carvel, the custom controller needs to be included with the helm templates. That is why the helm templates have extra files when compared to looking at upstream metacontroller.
 
 ### Releasing
 
---
+++ controller/main.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"path/filepath"
+	"slices"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+var secretGVR = schema.GroupVersionResource{
+	Group:    "",
+	Version:  "v1",
+	Resource: "secrets",
+}
+
+type StringSlice []string
+
+func (s *StringSlice) String() string {
+	return fmt.Sprintf("%v", *s)
+}
+
+func (s *StringSlice) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+type ArgoCluster struct {
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              *ArgoClusterSpec `json:"spec,omitempty"`
+}
+
+type ArgoClusterSpec struct {
+	ClusterName   string            `json:"clusterName"`
+	ArgoNamespace string            `json:"argoNamespace"`
+	ClusterLabels map[string]string `json:"clusterLabels,omitempty"`
+	Project       string            `json:"project"`
+}
+
+type ArgoClusterSecret struct {
+	Name             string `json:"name,omitempty"`
+	Server           string `json:"server,omitempty"`
+	Config           string `json:"config,omitempty"`
+	Project          string `json:"project,omitempty"`
+	ClusterResources string `json:"clusterResources,omitempty"`
+}
+
+type ArgoConfig struct {
+	TLSClientConfig *TLSClientConfig `json:"tlsClientConfig,omitempty"`
+}
+
+type TLSClientConfig struct {
+	CAData     string `json:"caData,omitempty"`     // base64-encoded PEM
+	CertData   string `json:"certData,omitempty"`   // base64-encoded PEM
+	KeyData    string `json:"keyData,omitempty"`    // base64-encoded PEM
+	Insecure   bool   `json:"insecure,omitempty"`   // skip TLS verification
+	ServerName string `json:"serverName,omitempty"` // optional SNI
+}
+
+func getSecret(client *dynamic.DynamicClient, namespace string, name string) (*unstructured.Unstructured, error) {
+
+	kubeconfigSecret, err := client.Resource(secretGVR).Namespace(namespace).Get(context.TODO(), fmt.Sprintf("%s-kubeconfig", name), metav1.GetOptions{})
+	if err != nil {
+		log.Printf("unable to retrive kubeconfig secret %v", err)
+		return nil, err
+	}
+	return kubeconfigSecret, nil
+
+}
+
+func convertObj(obj any) (ArgoCluster, error) {
+	argoCluster := ArgoCluster{}
+	unstructuredObj, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		return argoCluster, fmt.Errorf("unable to convert to unstuctured object")
+	}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredObj.Object, &argoCluster)
+	if err != nil {
+		return argoCluster, err
+	}
+	return argoCluster, nil
+}
+
+func applySecret(client *dynamic.DynamicClient, obj any, namespaces StringSlice) {
+	argoCluster, err := convertObj(obj)
+	if err != nil {
+		log.Printf("unable to convert object to structured argocd cluster: %v", err)
+		return
+	}
+
+	namespace := argoCluster.ObjectMeta.Namespace
+	clusterName := argoCluster.Spec.ClusterName
+	project := argoCluster.Spec.Project
+	labels := argoCluster.Spec.ClusterLabels
+	argoNamespace := argoCluster.Spec.ArgoNamespace
+
+	if slices.Contains(namespaces, argoNamespace) {
+		log.Printf("argoNamespace is in the list of blocked namespaces, not creating secret: %v", namespaces)
+		return
+	}
+
+	kubeconfigUns, err := getSecret(client, namespace, clusterName)
+	if err != nil {
+		log.Printf("unable to retrieve kubeconfig secret: %v", err)
+		return
+	}
+
+	kubeconfig, found, err := unstructured.NestedStringMap(kubeconfigUns.Object, "data")
+	if err != nil || !found {
+		log.Printf("cannot get secret data: %v", err)
+		return
+	}
+
+	encodedSecret, ok := kubeconfig["value"]
+	if !ok {
+		log.Println("value does not exist in kubeconfig secret")
+		return
+	}
+
+	decoded, err := base64.StdEncoding.DecodeString(encodedSecret)
+	if err != nil {
+		log.Printf("failed to decode value: %v", err)
+		return
+	}
+
+	config, err := clientcmd.Load(decoded)
+	if err != nil {
+		log.Printf("failed to read kubconfig data: %v", err)
+		return
+	}
+	argoConfig := &ArgoConfig{
+		TLSClientConfig: &TLSClientConfig{
+			CAData:   string(config.Clusters[clusterName].CertificateAuthorityData),
+			KeyData:  string(config.AuthInfos[fmt.Sprintf("%s-admin", clusterName)].ClientKeyData),
+			CertData: string(config.AuthInfos[fmt.Sprintf("%s-admin", clusterName)].ClientCertificateData),
+		},
+	}
+
+	jsonConfig, err := json.Marshal(argoConfig)
+	if err != nil {
+		log.Printf("unable to encoded argo config: %v", err)
+	}
+	secretData := &ArgoClusterSecret{
+		Name:             base64.StdEncoding.EncodeToString([]byte(clusterName)),
+		Server:           base64.StdEncoding.EncodeToString([]byte(config.Clusters[clusterName].Server)),
+		ClusterResources: base64.StdEncoding.EncodeToString([]byte("true")),
+		Project:          base64.StdEncoding.EncodeToString([]byte(project)),
+		Config:           base64.StdEncoding.EncodeToString([]byte(string(jsonConfig))),
+	}
+
+	secretName := fmt.Sprintf("%s-argo-cluster", clusterName)
+	secret := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Secret",
+			"metadata": map[string]interface{}{
+				"name":   secretName,
+				"labels": labels,
+			},
+			"type": "Opaque",
+			"data": secretData,
+		},
+	}
+
+	_, err = client.Resource(secretGVR).Namespace(argoNamespace).Apply(context.TODO(), secretName, secret, metav1.ApplyOptions{FieldManager: "argo-attach-controller"})
+	if err != nil {
+		log.Printf("unable to create or update argo cluster secret %v", err)
+		return
+	}
+	log.Printf("succesfully created or update argo cluster secret %s", secretName)
+}
+
+func deleteSecret(client *dynamic.DynamicClient, obj any) {
+	argoCluster, err := convertObj(obj)
+	if err != nil {
+		log.Printf("unable to convert object to structured argocd cluster: %v", err)
+		return
+	}
+
+	clusterName := argoCluster.Spec.ClusterName
+	secretName := fmt.Sprintf("%s-argo-cluster", clusterName)
+	argoNamespace := argoCluster.Spec.ArgoNamespace
+
+	err = client.Resource(secretGVR).Namespace(argoNamespace).Delete(context.TODO(), secretName, metav1.DeleteOptions{})
+	if err != nil {
+		log.Printf("unable to delete argo cluster secret %v", err)
+		return
+	}
+	log.Printf("succesfully deleted argo cluster secret %s", secretName)
+
+}
+
+func main() {
+	var namespaces StringSlice
+	flag.Var(&namespaces, "blocked-ns", "blocked namespaces , these namespaces will not be allowed as argo namespace options in the CR(can be specified multiple times)")
+	resync := flag.Int("resync-period", 60, "time in seconds")
+
+	flag.Parse() // parse flags
+
+	var kubeconfig string
+	if home := homedir.HomeDir(); home != "" {
+		fmt.Println("using local kubeconfig")
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		fmt.Println("Falling back to in-cluster config")
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	argoCluster := schema.GroupVersionResource{Group: "field.vmware.com", Version: "v1", Resource: "argoclusters"}
+
+	informer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return dynClient.Resource(argoCluster).Namespace("").List(context.TODO(), options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return dynClient.Resource(argoCluster).Namespace("").Watch(context.TODO(), options)
+			},
+		},
+		&unstructured.Unstructured{},
+		time.Duration(*resync)*time.Second,
+		cache.Indexers{},
+	)
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			fmt.Println("Add event detected:", obj)
+			//create a secret in the correct namespace
+			applySecret(dynClient, obj, namespaces)
+
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			fmt.Println("Update event detected:", newObj)
+			//update secret
+			applySecret(dynClient, newObj, namespaces)
+		},
+		DeleteFunc: func(obj interface{}) {
+			fmt.Println("Delete event detected:", obj)
+			//delete the secret
+			deleteSecret(dynClient, obj)
+		},
+	})
+
+	stop := make(chan struct{})
+	defer close(stop)
+
+	go informer.Run(stop)
+
+	if !cache.WaitForCacheSync(stop, informer.HasSynced) {
+		panic("Timeout waiting for cache sync")
+	}
+
+	fmt.Println("Argo Cluster Controller started successfully")
+
+	<-stop
+}
